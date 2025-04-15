dfc0f1a076d8becd5caaa4bbbe7705675d502398
thaufaniqbal <t.iqbal.a@gmail.com>
2025-04-15 22:12:04 +0700

    day 1 - soal 1

---
+++ src/topan/jawaban/day1/Day1Soal1.java
@@ -0,0 +1,16 @@
+package topan.jawaban.day1;
+
+public class Day1Soal1 {
+    public static void main (String [] args){
+        System.out.println ("arul");
+        System.out.println ("topan");
+        System.out.println ("monang");
+        System.out.println ("bang ayeng");
+        System.out.println ("bang iki");
+        System.out.println ("arul2");
+        System.out.println ("arul3");
+        System.out.println ("arul4");
+        System.out.println ("arul5");
+        System.out.println ("arul6");
+    }
+}
