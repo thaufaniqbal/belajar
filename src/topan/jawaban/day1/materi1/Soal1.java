a9182531903acb105dde28b087284df1a5720047
arul <arul.azzaky2901@gmail.com>
2025-04-15 22:39:46 +0700

    day1-soal1

---
+++ src/topan/jawaban/day1/materi1/Soal1.java
@@ -0,0 +1,17 @@
+package topan.jawaban.day1.materi1;
+
+public class Soal1{
+    public static void main (String[]args){
+        System.out.println("arul");
+        System.out.println("topan");
+        System.out.println("monang");
+        System.out.println("bang iki");
+        System.out.println("bang ayeng");
+        System.out.println("adnan");
+        System.out.println("rayhan");
+        System.out.println("agung");
+        System.out.println("abiyyu");
+        System.out.println("fathir");
+
+    }
+}
