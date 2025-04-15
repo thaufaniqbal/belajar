982d98d4176231d0afc1a7176d81f103d1820af7
arul <arul.azzaky2901@gmail.com>
2025-04-15 23:05:04 +0700

    day1-soal2

---
+++ src/topan/jawaban/day1/materi1/Soal2.java
@@ -0,0 +1,9 @@
+package topan.jawaban.day1.materi1;
+
+public class soal2{
+    public static void main (String[]args){
+        System.out.println("SMA NEGERI 1 CILEGON");
+        System.out.println("SMA NEGERI 2 KRAKATAU STEEL CILEGON");
+        System.out.println("SMK 4 PELAYARAN");
+    }
+}
