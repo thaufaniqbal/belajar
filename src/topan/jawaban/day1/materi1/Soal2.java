b36b6458259c07422c46602a3ddb7e974cb241fc
arul <arul.azzaky2901@gmail.com>
2025-04-15 23:14:29 +0700

    day1-soal2

--- src/topan/jawaban/day1/materi1/Soal2.java
+++ src/topan/jawaban/day1/materi1/Soal2.java
@@ -1,6 +1,6 @@
 package topan.jawaban.day1.materi1;
 
-public class soal2{
+public class Soal2{
     public static void main (String[]args){
         System.out.println("SMA NEGERI 1 CILEGON");
         System.out.println("SMA NEGERI 2 KRAKATAU STEEL CILEGON");
