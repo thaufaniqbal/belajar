3131a4240c01989150b00afa4cb3dfb452a59885
thaufaniqbal <t.iqbal.a@gmail.com>
2025-04-15 22:17:21 +0700

    day 1 - soal 1

---
+++ src/topan/jawaban/day1/materi1/Soal1.java
@@ -0,0 +1,15 @@
+package topan.jawaban.day1.materi1;
+
+public class Soal1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
