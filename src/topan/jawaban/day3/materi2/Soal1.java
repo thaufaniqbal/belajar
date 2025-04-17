fc3ca10ab2b6d2086d734ae86b85b706ca84d604
arul <arul.azzaky2901@gmail.com>
2025-04-18 03:25:18 +0700

    day3-materi2-soal1

---
+++ src/topan/jawaban/day3/materi2/Soal1.java
@@ -0,0 +1,20 @@
+package topan.jawaban.day3.materi2;
+
+public class Soal1 {
+    public static void main(String[] args){
+
+    int sisi = 9;
+    int rumus = 3;
+
+    int s = 0;
+    int r = 0;
+
+    s =+ sisi;
+    r =+ rumus;
+
+    long rumusKelilingSegitigaSamsis = s * r;
+
+    System.out.println("Hasilnya adalah: " + rumusKelilingSegitigaSamsis);
+}
+}
+//cihuyy cihuyy alhamdulillahh wkwkkwk
