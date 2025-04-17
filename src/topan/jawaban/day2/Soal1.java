dbb907ffdde88564ede9f73d3f2d96315d316fd0
arul <arul.azzaky2901@gmail.com>
2025-04-17 23:56:28 +0700

    day2-soal1

--- src/topan/jawaban/day2/Soal1.java
+++ src/topan/jawaban/day2/Soal1.java
@@ -4,8 +4,8 @@ public class Soal1{
     public static void main(String[] args){
         //tipeData $namaVariabel = nilaiVariabel;
         String iniString = "arul";
-        int iniInteger = 21;
-        long iniLong = 7;
+        int iniInteger = 22;
+        long iniLong = 5;
         float iniFloat = 8.76694f;
         double iniDouble = 8.435;
         char iniChar = 'a';
@@ -13,12 +13,28 @@ public class Soal1{
         boolean iniBooleanFalse = false;
 
        System.out.println(iniString);
+       System.out.println(iniString);
+       System.out.println(iniString);
+       System.out.println(iniInteger);
+       System.out.println(iniInteger);
        System.out.println(iniInteger);
        System.out.println(iniLong);
+       System.out.println(iniLong);
+       System.out.println(iniLong);
+       System.out.println(iniFloat);
+       System.out.println(iniFloat);
        System.out.println(iniFloat);
        System.out.println(iniDouble);
+       System.out.println(iniDouble);
+       System.out.println(iniDouble);
+       System.out.println(iniChar);
+       System.out.println(iniChar);
        System.out.println(iniChar);
        System.out.println(iniBooleanTrue);
+       System.out.println(iniBooleanTrue);
+       System.out.println(iniBooleanTrue);
+       System.out.println(iniBooleanFalse);
+       System.out.println(iniBooleanFalse);
        System.out.println(iniBooleanFalse);
     }
 }
