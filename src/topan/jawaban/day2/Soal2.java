17423dcffb98f155a823b5827f2a8a45964c1a7c
arul <arul.azzaky2901@gmail.com>
2025-04-18 00:38:48 +0700

    day2-soal2

---
+++ src/topan/jawaban/day2/Soal2.java
@@ -0,0 +1,20 @@
+package topan.soal.day2;
+
+public class Soal2{
+    public static void main (String[] args){
+        //tipeData $namaVariabel = nilaiVariabel
+        String nama = "arul";
+        int umur = 17;
+        long lingkar = 59;
+        float tinggi = 165;
+        double berat = 76.5;
+        char golongan = 'o';
+        boolean tebak = true;
+        boolean tebakk = false;
+        System.out.println("nama saya: " + nama + " umur saya: " + umur + " lingkar kepala saya: " + lingkar
+                + " tinggi badan saya: " + tinggi + 14 + " berat badan saya: " + (berat + 5)
+    + " golongan darah saya: " + golongan + " pilihan cuy: " + tebak + " pilihan lagi nii: " +
+    tebakk);
+    }
+
+}
