ebbe5749d366b3cfcb4693989ef37b4418df68a8
arul <arul.azzaky2901@gmail.com>
2025-04-18 03:04:15 +0700

    day3-soal2

---
+++ src/topan/iseng/Train1.java
@@ -0,0 +1,9 @@
+package topan.iseng;
+
+public class Train1{
+    public static void main(String[] args){
+        String udud = "Marlong";
+        System.out.println(udud + "isi 20");
+        System.out.println("oh yeah babyy" + " rawwwrrrrr");
+    }
+}
---
+++ src/topan/jawaban/day3/materi1/Soal2.java
@@ -0,0 +1,25 @@
+package topan.jawaban.day3.materi1;
+
+public class Soal2{
+    public static void main (String[] args) {
+        int angka1 = 10;
+                int angka2 = 8;
+                int angka3 = 9;
+                int angka4 = 3;
+                int angka5 = 7;
+                int angka6 = 18;
+                int angka7 = 14;
+                int angka8 = 16;
+
+                //NGITUNGG!!!!!
+        int perkalian1 = angka5 * angka1;
+        int perkalian2 = angka2 * angka4;
+        int increment1 = angka3++;
+        int increment2 = angka6++;
+        int decrement1 = angka7--;
+                int decrement2 = angka1--;
+
+                System.out.println(perkalian1 + perkalian2 - increment1 * increment2 + decrement1 % decrement2);
+    }
+}
+//mantap ga coachh wkkwkwkw
