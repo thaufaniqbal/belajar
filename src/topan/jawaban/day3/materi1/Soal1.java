398ad5eb47517fa6a378c4af1731f0482f4092ec
arul <arul.azzaky2901@gmail.com>
2025-04-18 02:25:18 +0700

    day3-soal1

---
+++ src/topan/jawaban/day3/materi1/Soal1.java
@@ -0,0 +1,17 @@
+package topan.jawaban.day3.materi1;
+
+public class Soal1 {
+    public static void main(String[] args) {
+        int nilai1 = 30;
+        int nilai2 = 50;
+        int nilai3 = 10;
+        int nilai4 = 70;
+
+//operator Aritmethic
+        int pertambahan = nilai1 + nilai3;
+        int perkalian = nilai2 * nilai1;
+        int pengurangan = nilai4 - nilai1;
+
+        System.out.println(pertambahan + perkalian + pengurangan);
+    }
+}
