982458850d8e6ef54e1598ce085577435156024d
thaufaniqbal <t.iqbal.a@gmail.com>
2025-04-18 21:52:02 +0700

    latihan day 1-3, approved

--- src/topan/iseng/Train4.java
+++ src/topan/iseng/Train4.java
@@ -8,6 +8,16 @@ public class Train4{
         int sisiTiga = 5;
         int segitigaSamsis = sisiSatu * sisiDua * sisiTiga;
 
-        System.out.println("Sisi A: " + sisiSatu + "Sisi B: " + sisiDua + "Sisi C: " + sisiTiga + "Caranya: A*B*C " + "Dan hasilnya adalah" + segitigaSamsis);
+        System.out.println("Sisi A: " + sisiSatu + " Sisi B: " + sisiDua + "Sisi C: " + sisiTiga + " Caranya: A*B*C " + "Dan hasilnya adalah " + segitigaSamsis);
+
+        sisiSatu = 10;
+        sisiDua = 10;
+        sisiTiga = 10;
+        segitigaSamsis = sisiSatu * sisiDua * sisiTiga;
+        System.out.println("Sisi A: " + sisiSatu + " Sisi B: " + sisiDua + "Sisi C: " + sisiTiga + " Caranya: A*B*C " + "Dan hasilnya adalah " + segitigaSamsis);
+
+        sisiTiga = 20;
+        segitigaSamsis = sisiSatu * sisiDua * sisiTiga;
+        System.out.println("Sisi A: " + sisiSatu + " Sisi B: " + sisiDua + "Sisi C: " + sisiTiga + " Caranya: A*B*C " + "Dan hasilnya adalah " + segitigaSamsis);
     }
 }
