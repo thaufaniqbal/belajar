c40eeadc2f59efc11ebe42d2943ab67803e12a9b
arul <arul.azzaky2901@gmail.com>
2025-04-18 21:25:56 +0700

    day3-materi2-soal2

---
+++ src/topan/jawaban/day3/materi2/Soal2.java
@@ -0,0 +1,13 @@
+package topan.jawaban.day3.materi2;
+
+public class Soal2{
+    public static void main(String[] args){
+        float phi = 3.14f;
+        int diameter = 40;
+
+
+        float kelilingLingkaran = phi*diameter;
+
+        System.out.println(kelilingLingkaran);
+    }
+}
