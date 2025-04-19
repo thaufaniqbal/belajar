4bc49b6b86d1668b3da8626e3462fec3a384dc3f
arul <arul.azzaky2901@gmail.com>
2025-04-19 23:56:16 +0700

    day4 - materi1 - soal1

--- src/topan/jawaban/day4/materi1/Soal1.java
+++ src/topan/jawaban/day4/materi1/Soal1.java
@@ -9,15 +9,17 @@ public class Soal1{
         nilaiDua = 30;
 
         boolean kurangDari = nilaiSatu < nilaiDua;
-        boolean lebihDari = nilaiSatu > nilaiDua;
-        boolean samaDengan = nilaiDua == nilaiSatu;
 
         System.out.println( "Nilai satu cuy: " + nilaiSatu + " Nilai Dua ya: " + nilaiDua + ", " + nilaiSatu + " Kurang dari " + nilaiDua + " = " + kurangDari);
+
         nilaiSatu = 691;
         nilaiDua = 961;
+        boolean lebihDari = nilaiSatu > nilaiDua;
         System.out.println( "Nilai satu nich: " + nilaiSatu + " Nilai Duah: " + nilaiDua + ", " + nilaiSatu + " Lebih dari " + nilaiDua + " = " + lebihDari);
+
         nilaiSatu = 1453;
         nilaiDua = 8755;
+        boolean samaDengan = nilaiDua == nilaiSatu;
         System.out.println( "Nilai satuch: " + nilaiSatu + " Nilai Duach: " + nilaiDua + ", " + nilaiSatu + " Sama dengan " + nilaiDua + " = " + samaDengan);
     }
 }
---
+++ src/topan/jawaban/day4/materi2/Soal1.java
@@ -0,0 +1,13 @@
+package topan.jawaban.day4.materi2;
+
+public class Soal1{
+    public static void main (String[] args) {
+
+        int nilaiSatu, nilaiDua;
+
+        nilaiSatu = 41;
+        nilaiDua = 51;
+
+
+    }
+}
