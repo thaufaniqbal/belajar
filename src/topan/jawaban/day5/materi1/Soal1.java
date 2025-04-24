81e7f28e8cec7b5a252bd23ef810d0a93ab38c28
arul <arul.azzaky2901@gmail.com>
2025-04-24 23:20:42 +0700

    day5 - materi1 - soal1

---
+++ src/topan/jawaban/day5/materi1/Soal1.java
@@ -0,0 +1,18 @@
+package topan.jawaban.day5.materi1;
+
+public class Soal1{
+
+    public static void main(String[] args){
+
+        int nilaiSatu = 70;
+        int nilaiDua = 85;
+
+        if (nilaiSatu <= nilaiDua) {
+            System.out.println("nilaiSatu lebih kecil atau sama dengan nilaiDua");
+        } else {
+            System.out.println("nilaiSatu lebih besar dari nilaiDua");
+        }
+    }
+}
+//bismillah bner, sakit kepala gw ngulik sana sini searching sana sini mahamin.
+//tpi masi kurang yakin untuk ngerjain soal spontan coach jdi mungkin penjelasan tambahan kali yaa
