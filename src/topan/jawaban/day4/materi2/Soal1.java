162fb96d865216c9e819ce370aee7f6636c484f6
arul <arul.azzaky2901@gmail.com>
2025-04-24 23:00:51 +0700

    day4 - materi2 - soal1

--- src/topan/jawaban/day4/materi2/Soal1.java
+++ src/topan/jawaban/day4/materi2/Soal1.java
@@ -5,9 +5,17 @@ public class Soal1{
 
         int nilaiSatu, nilaiDua;
 
-        nilaiSatu = 41;
-        nilaiDua = 51;
+        nilaiSatu = 20;
+        nilaiDua = 40;
 
+        int perkalian = nilaiSatu * 2;
 
+        boolean bandingkanPerkalian = nilaiDua == perkalian;
+        boolean hasil = bandingkanPerkalian && nilaiSatu != nilaiDua;
+
+        System.out.println(bandingkanPerkalian);
+        System.out.println(hasil);
     }
 }
+
+//gini gasi coach, maaf yaa lama banget coach. harus bertarung sma tugas sekolah juga yang bejibun coach
